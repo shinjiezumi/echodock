08fa75cfccb31608a2f99e05ec4273afaea72bd8
shinjiezumi <shinjiezumi.work@gmail.com>
2020-09-21 23:44:08 +0900

    詳細ページの改行対応

--- src/views/board/main.go
+++ src/views/board/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"html/template"
 	"io"
+	"strings"
 )
 
 const basePath = "views/board/"
@@ -30,9 +31,16 @@ func LoadTemplate(e *echo.Echo) {
 		basePath + "form.html",
 	}
 
+	funcs := template.FuncMap{
+		"nl2br": func(text string) template.HTML {
+			return template.HTML(strings.Replace(template.HTMLEscapeString(text), "\n", "<br>", -1))
+		},
+	}
+
 	templates := make(map[string]*template.Template)
 	for _, v := range pages {
-		templates[v] = template.Must(template.ParseFiles(append(commonTemplates, basePath+v+".html")...))
+		templates[v] = template.Must(template.New("t").Funcs(funcs).ParseFiles(append(commonTemplates, basePath+v+".html")...))
+		templates[v].Funcs(funcs)
 	}
 
 	t := &Template{
