80af9bbe7b5552bc98577810ba9e8093cb9569b0
shinjiezumi <shinjiezumi.work@gmail.com>
2020-09-28 21:36:42 +0900

    トップページ実装

--- src/go.mod
+++ src/go.mod
@@ -5,10 +5,9 @@ go 1.13
 require (
 	github.com/gorilla/sessions v1.2.1
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo-contrib v0.9.0
 	github.com/labstack/echo/v4 v4.1.6
-	github.com/labstack/gommon v0.3.0
+	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/valyala/fasttemplate v1.2.1 // indirect
--- src/views/main.go
+++ src/views/main.go
@@ -4,19 +4,62 @@ import (
 	"github.com/labstack/echo/v4"
 	"html/template"
 	"io"
+	"strings"
 )
 
+const (
+	basePath    = "views/"
+	boardPath   = "views/board/"
+	commentPath = "views/board/comment/"
+)
+
+var pages = []string{
+	"top",
+	"hello",
+}
+
+var boardPages = []string{
+	"index",
+	"create",
+	"show",
+	"edit",
+}
+
 type Template struct {
-	templates *template.Template
+	templates map[string]*template.Template
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, _ echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
+	return t.templates[name].ExecuteTemplate(w, "layout.html", data)
 }
 
 func LoadTemplate(e *echo.Echo) {
+	commonTemplates := []string{
+		basePath + "layout.html",
+		basePath + "header.html",
+		boardPath + "form.html",
+		commentPath + "comment.html",
+		commentPath + "form.html",
+	}
+
+	funcs := template.FuncMap{
+		"nl2br": func(text string) template.HTML {
+			return template.HTML(strings.Replace(template.HTMLEscapeString(text), "\n", "<br>", -1))
+		},
+	}
+
+	templates := make(map[string]*template.Template)
+	for _, v := range pages {
+		templates[v] = template.Must(template.New("t").Funcs(funcs).ParseFiles(append(commonTemplates, basePath+v+".html")...))
+		templates[v].Funcs(funcs)
+	}
+	for _, v := range boardPages {
+		templates[v] = template.Must(template.New("t").Funcs(funcs).ParseFiles(append(commonTemplates, boardPath+v+".html")...))
+		templates[v].Funcs(funcs)
+	}
+
 	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+		templates: templates,
 	}
 
 	e.Renderer = t
