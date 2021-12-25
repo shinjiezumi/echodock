bee0cf0e09925dba3a9df3a0709f66a37c6077f8
shinjiezumi <shinjiezumi.work@gmail.com>
2021-12-26 00:52:05 +0900

    Refactor echo basic routing

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -14,9 +13,7 @@ import (
 
 	"echodock/board"
 	"echodock/database"
-	"echodock/echobasic/cookie"
-	"echodock/echobasic/request"
-	"echodock/echobasic/response"
+	"echodock/echobasic"
 	"echodock/localstack"
 	"echodock/views"
 )
@@ -65,38 +62,8 @@ func main() {
 		return c.String(http.StatusOK, "ok")
 	})
 
-	// クエリパラメータの取得
-	e.GET("/request/query", request.HandleQuery)
-
-	// URLパラメータの取得
-	e.GET("/request/get/:id", request.HandleGet)
-
-	// POSTパラメータの取得
-	e.POST("/request/post/:id", request.HandlePost)
-
-	// 文字列レスポンス
-	e.GET("/response/string", response.StringResponse)
-
-	// HTMLレスポンス
-	e.GET("/response/html", response.HtmlResponse)
-
-	// HTML(テンプレートファイル)レスポンス
-	e.GET("/response/htmltemplate", response.HtmlTemplateResponse)
-
-	// Cookieの取得と設定
-	e.GET("/cookie", cookie.HandleCookie)
-
-	// 外部リソースの取得
-	e.GET("/search/:query", func(c echo.Context) error {
-		endpoint := "https://google.com/search?q=" + c.Param("query")
-		req, _ := http.NewRequest("GET", endpoint, nil)
-
-		var client = &http.Client{}
-		resp, _ := client.Do(req)
-		body, _ := ioutil.ReadAll(resp.Body)
-
-		return c.HTML(http.StatusOK, string(body))
-	})
+	// echo基本機能
+	echobasic.SetUpRoutes(e)
 
 	// 掲示板
 	board.Setup(e)
