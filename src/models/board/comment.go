b19444d4bbf2f153e9044b289f0a0187a558e2cb
shinjiezumi <shinjiezumi.work@gmail.com>
2022-01-07 22:29:39 +0900

    Add comment

--- src/models/board/comment.go
+++ src/models/board/comment.go
@@ -4,15 +4,17 @@ import (
 	"time"
 )
 
+// Comment コメント
 type Comment struct {
-	ID        int `gorm:"primaryKey"`
-	BoardID   int
-	Comment   string
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `gorm:"primaryKey"` // ID
+	BoardID   int       // 投稿ID
+	Comment   string    // コメント
+	Name      string    // 投稿者名
+	CreatedAt time.Time // 作成日時
+	UpdatedAt time.Time // 更新日時
 }
 
+// TableName はテーブル名を返します
 func (b Comment) TableName() string {
 	return "comments"
 }
